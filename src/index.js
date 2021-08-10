13083d81f7968401604ac6b581620d2f2eb7daf4
Ekow Bentsi-Enchill <ekow.bentsi.enchill@gmail.com>
2021-08-10 18:28:05 +0000

    start homepage view

--- src/index.js
+++ src/index.js
@@ -5,14 +5,18 @@ document.addEventListener("DOMContentLoaded", () => {
     // home page
     fetch(URL_PREFIX + 'sports')
         .then(r => r.json())
-        .then(renderSports)
+        .then(sports => {
+
+            renderSports(sports)
+        })
 
 
     document.addEventListener("click", e => {
-
+        //log to check what I've clicked
         console.log(`${e.target.className} clicked`)
+        
         // when a sport is clicked, go to sport landing page
-        if (e.target.className == 'sport-header') {
+        if (e.target.classList.contains('sport-header')) {
 
             const sport_id = e.target.dataset.id
 
@@ -243,17 +247,48 @@ function renderSports(sports) {
     let sportsPanel = document.querySelector("div#sports-panel")
     sportsPanel.style.display = "block"
 
+    // the following code sets up the sports in nice boxes. it's quite pretty
+    let sportsContainer = sportsPanel.querySelector('div')
+    
+    let sportsRow = sportsContainer.querySelector('div')
+
     sports.forEach(sport => {
-        // name
-        let nameTag = document.createElement('h2')
-        nameTag.dataset.id = sport.id
-        nameTag.className = 'sport-header'
-        nameTag.textContent = `${sport.name}`
+        // code for bordering and prettiness
+        let sportColumn = document.createElement('div')
+        sportColumn.className = "col-md-4"
+
+        let sportCard = document.createElement('div')
+        sportCard.classList.add("card", "mb-4", "box-shadow", "text-white")
+
+        let sportImg = document.createElement('img')
+        sportImg.dataset.src = "holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail"
+        sportImg.style.height = "225px"
+        sportImg.style.width = "100%"
+        sportImg.src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22208%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20208%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17b30b02b30%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A11pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17b30b02b30%22%3E%3Crect%20width%3D%22208%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2266.9296875%22%20y%3D%22117.45%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
+
+        let cardBody = document.createElement('div')
+        cardBody.classList.add("card-img-overlay")
+        
+        let cardTitle = document.createElement('h5')
+        cardTitle.className = "card-title"
+        cardTitle.textContent = `${sport.name}`
+        
+        let cardStretchedLink = document.createElement('a')
+        cardStretchedLink.href = "#"
+        cardStretchedLink.classList.add("stretched-link", "sport-header")
+        cardStretchedLink.dataset.id = sport.id
 
         // add to doc
-        sportsPanel.appendChild(nameTag)
-
+        cardBody.appendChild(cardTitle)
+        cardBody.appendChild(cardStretchedLink)
+        sportCard.appendChild(sportImg)
+        sportCard.appendChild(cardBody)
+        sportColumn.appendChild(sportCard)
+        sportsRow.appendChild(sportColumn)
     });
+
+    sportsContainer.appendChild(sportsRow)
+    sportsPanel.appendChild(sportsContainer)
 
 }
 
